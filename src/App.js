2b1dc9a51630d2337d063c7c5e917f68fb7ef599
stavLidor <stav45451998@walla.com>
2022-02-28 01:19:52 +0200

    firebase add user,patients. login to user in firebase only according email and password.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Patient from "./pages/patient/Patient"
 
 import db from './firebase';
-import { doc,getDocs , onSnapshot, collection, query, where } from "firebase/firestore";
+// import { doc,getDocs , onSnapshot, collection, query, where } from "firebase/firestore";
+import { collection, doc, setDoc,getDoc ,where,query, getDocs} from "firebase/firestore";
+// import {  doc,getDocFromCache} from "firebase/firestore";
 
 
 import React, { useState, useEffect, Component }  from 'react';
@@ -17,33 +19,36 @@ import {
 import NewUser from "./components/login/NewUser";
 
 function App() {
-    const collection_query = collection(db,"institutes");
+    const collection_query_users = collection(db,"users");
+    const collection_query_patients = collection(db,"patients");
     const [institutes, setInstitutes] = useState([]);
     const [loading, setLoading] = useState(false);
 
     const [user,setUser] = useState({email:"",password:""});
+    const [newUser,setNewUser] = useState({name:"",type:"",email:"",password:"",patients:[]});
+    const [newPatients,setNewPatients] = useState({id:"",name:""});
     const [error,setError] = useState("");
-    const [newUser,setNewUser]=useState(false);
+    const [newUserFlag,setNewUserFlag]=useState(false);
 
-    function getInstitutes() {
-        setLoading(true);
-        // const querySnapshot = await getDocs(collection(db, "users"));
-        // querySnapshot.forEach((doc) => {
-        //     console.log(`${doc.id} => ${doc.data()}`);
-        // });
-        onSnapshot(collection_query,(querySnapshot)=>{
-            const items = [];
-            querySnapshot.forEach((doc) => {
-                items.push(doc.data());
-            });
-            setInstitutes(items);
-        setLoading(false);
-        })
-    }
+    // function getInstitutes() {
+    //     setLoading(true);
+    //     // const querySnapshot = await getDocs(collection(db, "users"));
+    //     // querySnapshot.forEach((doc) => {
+    //     //     console.log(`${doc.id} => ${doc.data()}`);
+    //     // });
+    //     onSnapshot(collection_query,(querySnapshot)=>{
+    //         const items = [];
+    //         querySnapshot.forEach((doc) => {
+    //             items.push(doc.data());
+    //         });
+    //         setInstitutes(items);
+    //     setLoading(false);
+    //     })
+    // }
 
-    useEffect(()=> {
-        getInstitutes();
-    }, []);
+    // useEffect(()=> {
+    //     getInstitutes();
+    // }, []);
 
     if (loading){
         return <h1>Loading...</h1>
@@ -56,27 +61,90 @@ function App() {
         password:"admin123"
     }
 
-    const Login = details=>{
+    const Login = async details=>{
+        // later check before if this in Cache
         console.log(details);
-        if (details.email === adminUser.email && details.password === adminUser.password){
-            console.log("Logged in")
-            setUser({
-                name:"Stav",
-                email: details.email
-            })
-        }
-        else{
+        const docRef = doc(db, "users", details.email);
+        try {
+            const doc = await getDoc(docRef);
+                if (doc.data().password === details.password){
+                    console.log("Logged in")
+                    setUser({
+                        name:doc.data().name,
+                        email: details.email
+                    })
+
+                }
+                else {
+                    console.log("Details do not match")
+                }
+            // Document was found in the cache. If no cached document exists,
+            // an error will be returned to the 'catch' block below.
+
+            console.log("Cached document data:", doc.data());
+        } catch (e) {
             console.log("Details do not match")
         }
+
+        // if (details.email === adminUser.email && details.password === adminUser.password){
+        //     console.log("Logged in")
+        //     setUser({
+        //         name:"Stav",
+        //         email: details.email
+        //     })
+        // }
+        // else{
+        //     console.log("Details do not match")
+        // }
 
     }
     const Logout = ()=>{
         // console.log('logout');
         setUser({email:"",password:""});
     }
-    const new_user= ()=>{
-        setNewUser(true);
-        console.log('yess');
+
+    const new_user=async details=>{
+        //setNewUserFlag(true);
+        // setNewUser({
+        //     name:details.name,type:details.type,email:details.email,password:details.password,patients:[]
+        //
+        // })
+        let arr_ids =details.ids.split(",");
+
+        await setDoc(doc(collection_query_users, details.email), {
+
+            name:details.name,type:details.type,email:details.email,password:details.password,ids:arr_ids});
+        // collection_query_users.doc("1")
+        //     .set({name:details.name,type:details.type,email:details.email,password:details.password,createdAt: db.firestore.FieldValue.serverTimestamp(),
+        //         lastUpdate: db._firestoreClient.serverTimestamp()}).catch((err)=>{alert(err)
+        // console.log(err)})
+        // await Promise.all([
+        //     setDoc(doc(citiesRef, 'SF', 'landmarks'), {
+        //         name: 'Golden Gate Bridge',
+        //         type: 'bridge'
+        //     })]);
+        console.log("HI user",details.name.toString(),details.type.toString(),details.password.toString());
+
+    }
+    const new_patients= async details=>{
+        //setNewUserFlag(true);
+        // const q = query(collection_query_users, where("ids", "in", details.id));
+        //const querySnapshot = await getDocs(q);
+        let mail_parents=[]
+        let mail_Therapist=[]
+        // querySnapshot.forEach((doc) => {
+        //     if (doc.data().type)
+        //     // doc.data() is never undefined for query doc snapshots
+        //     //console.log(doc.id, " => ", doc.data());
+        // });
+
+        await setDoc(doc(collection_query_patients, details.id), {
+
+            id:details.id,name:details.name});
+        // setNewUser({
+        //     id:details.id,name:details.name
+        // })
+        console.log("HI",details.id.toString(),details.name.toString());
     }
   return (
 
@@ -115,13 +183,13 @@ function App() {
             </div>
 
         ):
-            (newUser === true) ? (
-                <div>
-                                    <NewUser/>
-                </div>
-            ):
+            // (newUserFlag === true) ? (
+            //     <div>
+            //                         <NewUser/>
+            //     </div>
+            // ):
             (
-            <LoginFrom Login={Login} error={error} new_user={new_user}/>
+            <LoginFrom Login={Login} error={error} new_user={new_user} new_patients={new_patients}/>
         )
         }
 
--- src/components/login/LoginFrom.js
+++ src/components/login/LoginFrom.js
@@ -2,19 +2,30 @@
 import React,{useState} from "react";
 
 
-export default function LoginFrom({Login,error,new_user}){
+export default function LoginFrom({Login,error,new_user,new_patients}){
     const [details,setDetails] = useState({email:"",password:""});
+    const [detailsNewUser,setDetailsNewUser] = useState({name:"",type:"therapist",email:"",password:"",patients:""});
+    const [detailsPatients,setDetailsPatients] = useState({id:"",name:""});
     const submitHandler=e=>{
         e.preventDefault();
         Login(details);
     }
     const submitNewUser=e=>{
         e.preventDefault();
-        new_user();
+        new_user(detailsNewUser);
+        // Login(details);
+    }
+    const submitNewPatient=e=>{
+        e.preventDefault();
+        new_patients(detailsPatients);
         // Login(details);
     }
     return(
         <div>
+            <h2>
+                עמוד מזכירה!
+            </h2>
+
         <form onSubmit={submitHandler}>
             <div className="from-inner">
                 <h2>
@@ -38,7 +49,53 @@ export default function LoginFrom({Login,error,new_user}){
             </div>
         </form >
             <form onSubmit={submitNewUser}>
+                <h2>
+                    מטפל או הורה חדש
+                </h2>
+                <div className="form-group" >
+                    <label htmlFor="name">שם:</label>
+                    <input type="text" name="name1" id="name1"onChange={e=>setDetailsNewUser({...detailsNewUser,name:e.target.value})} value={detailsNewUser.name}/>
+                </div>
+                {/*<select id="type" onChange={e=>setDetailsNewUser({...detailsNewUser,type:e.target.value})} value={details.type}>*/}
+                {/*    <label htmlFor="name">סוג:</label>*/}
+                {/*    <option value="0">מטפל</option>*/}
+                {/*    <option value="1">הורה</option>*/}
+                {/*</select>*/}
+                <label>סוג:
+                    <select type="text" name="type" id="type" onChange={e=>setDetailsNewUser({...detailsNewUser,type:e.target.value})} value={detailsNewUser.type} >
+                            <option value="therapist">מטפל</option>
+                            <option value="parent">הורה</option>
+                    </select>
+                </label>
+                <div className="form-group">
+                    <label htmlFor="email">איימיל:</label>
+                    <input type="email" name="email" id="email" onChange={e=>setDetailsNewUser({...detailsNewUser,email:e.target.value})} value={detailsNewUser.email}/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">סיסמא:</label>
+                    <input type="password" name="password" id="password" onChange={e=>setDetailsNewUser({...detailsNewUser,password:e.target.value})} value={detailsNewUser.password}/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="ids">תעודות זהות של הילדים:</label>
+                    <input type="text" name="ids" id="ids" onChange={e=>setDetailsNewUser({...detailsNewUser,ids:e.target.value})} value={detailsNewUser.ids}/>
+                </div>
                 <input type="submit" value="משתמש חדש"/>
+            </form>
+            <form onSubmit={submitNewPatient} >
+                <h2>
+                    מטופל חדש
+                </h2>
+                <div className="form-group">
+                    <label htmlFor="id">תעודות זהות של המטופל:</label>
+                    <input type="number" name="id" id="id" onChange={e=>setDetailsPatients({...detailsPatients,id:e.target.value})} value={detailsPatients.ids}/>
+                </div>
+
+                <div className="form-group" >
+                    <label htmlFor="name">שם:</label>
+                    <input type="text" name="name" id="name" onChange={e=>setDetailsPatients({...detailsPatients,name:e.target.value})} value={detailsPatients.name}/>
+                </div>
+
+                <input type="submit" value="מטופל חדש"/>
             </form>
         </div>
 

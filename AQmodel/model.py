9c3dca39bd86394503b55fc97dcb7e29f72ba30d
Ronli Vignanski <ronli.vignanski@live.biu.ac.il>
2022-04-24 23:44:01 +0300

    writing the machine learning model.

---
+++ AQmodel/model.py
@@ -0,0 +1,150 @@
+# Importing libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Suppress warnings
+import warnings
+
+warnings.filterwarnings("ignore")
+
+# Classification
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+
+# Modelling Helpers :
+from sklearn.model_selection import train_test_split
+
+# Preprocessing :
+from sklearn.preprocessing import LabelEncoder
+
+# Metrics :
+
+# Classification
+from sklearn.metrics import accuracy_score, classification_report
+# confusion matrix
+from sklearn.metrics import classification_report, confusion_matrix
+# logistic method
+from sklearn.linear_model import LogisticRegression
+# Random forest
+from sklearn.ensemble import RandomForestClassifier
+# SVM
+from sklearn.svm import SVC
+
+
+# read the csv file
+asd_data = pd.read_csv("./input/Toddler Autism dataset July 2018.csv")
+
+# get rid of the data we do not need
+asd_data.drop(['Case_No', 'Who completed the test'], axis=1, inplace=True)
+
+label_encoder = LabelEncoder()
+columns = ['Class/ASD Traits ', 'Family_mem_with_ASD',  'Jaundice', 'Ethnicity', 'Sex',]
+for col in columns:
+    asd_data[col] = label_encoder.fit_transform(asd_data[col])
+
+
+X = asd_data.drop(['Class/ASD Traits '], axis=1)
+Y = asd_data['Class/ASD Traits ']
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.20, random_state=7)
+
+
+###################### FIRST MODEL ######################
+
+# KNN
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(x_train, y_train)
+pred = knn.predict(x_test)
+
+classification_report(y_test, pred)
+
+
+# find the best k in knn, which means the optimum number of neighbors to consider
+error = []
+for i in range(1, 40):
+    knn = KNeighborsClassifier(n_neighbors=i)
+    knn.fit(x_train, y_train)
+    pred = knn.predict(x_test)
+    error.append(np.mean(y_test != pred))
+# plt.figure(figs=(10, 10))
+# plt.plot(range(1, 40), error, color='green', linestyle='dashed', marker='o', marketplace='red')
+
+knn = KNeighborsClassifier(n_neighbors=27)
+knn.fit(x_train, y_train)
+pred = knn.predict(x_test)
+classification_report(y_test, pred)
+
+confusion_matrix(y_test, pred)
+
+#########################################################
+
+
+
+###################### SECOND MODEL ######################
+
+# logistic regression
+log_reg = LogisticRegression()
+log_reg.fit(x_train, y_train)
+prediction = log_reg.predict(x_test)
+log_reg.score(x_train, y_train)
+
+classification_report(y_test, prediction)
+
+#########################################################
+
+
+
+###################### THIRD MODEL ######################
+
+# decision tree
+d_tree = DecisionTreeClassifier()
+d_tree.fit(x_train, y_train)
+pred = d_tree.predict(x_test)
+classification_report(y_test, pred)
+
+#########################################################
+
+
+
+###################### FOURTH MODEL ######################
+
+# Random forest
+random_forest = RandomForestClassifier()
+random_forest.fit(x_train, y_train)
+pred = random_forest.predict(x_test)
+classification_report(y_test, pred)
+
+confusion_matrix(y_test, pred)
+
+#########################################################
+
+
+
+###################### FIFTH MODEL ######################
+
+# SVM
+model = SVC()
+model.fit(x_train, y_train)
+pred = model.predict(x_test)
+classification_report(y_test, pred)
+
+confusion_matrix(y_test, pred)
+
+#########################################################
+
+
+# check all the models in order to get the one with the best results
+models = []
+
+models.append(('KNN                 :', KNeighborsClassifier()))
+models.append(('Logistic Regression :', LogisticRegression()))
+models.append(('Naive Bayes         :', GaussianNB()))
+models.append(('SVM                 :', SVC()))
+models.append(('Decision Tree       :', DecisionTreeClassifier()))
+models.append(('Random forest       :', RandomForestClassifier()))
+
+for name, model in models:
+    model.fit(x_train, y_train)
+    pred = model.predict(x_test).astype(int)
+    print(name, accuracy_score(y_test, pred))
--- src/App.js
+++ src/App.js
@@ -1,159 +1,53 @@
-import Topbar from "./components/topbar/Topbar";
-import Sidebar from "./components/sidebar/Sidebar";
-import Patient from "./pages/patient/Patient"
-
-import db from './firebase';
-import { doc,getDocs , onSnapshot, collection, query, where } from "firebase/firestore";
-
-
-import React, { useState, useEffect, Component }  from 'react';
-import LoginFrom from "./components/login/LoginFrom";
+import React from 'react';
 import "./app.css"
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route
-} from 'react-router-dom';
-import NewUser from "./components/login/NewUser";
+import withDirection, { withDirectionPropTypes, DIRECTIONS } from 'react-with-direction';
+
+import LoginFrom from "./components/login/LoginFrom";
+import Chat from "./components/chats/Chat";
 
 function App() {
-    const collection_query = collection(db,"institutes");
-    const [institutes, setInstitutes] = useState([]);
-    const [loading, setLoading] = useState(false);
+    // function getInstitutes() {
+    //     setLoading(true);
+    //     // const querySnapshot = await getDocs(collection(db, "users"));
+    //     // querySnapshot.forEach((doc) => {
+    //     //     console.log(`${doc.id} => ${doc.data()}`);
+    //     // });
+    //     onSnapshot(collection_query,(querySnapshot)=>{
+    //         const items = [];
+    //         querySnapshot.forEach((doc) => {
+    //             items.push(doc.data());
+    //         });
+    //         setInstitutes(items);
+    //     setLoading(false);
+    //     })
+    // }
 
-    const [user,setUser] = useState({email:"",password:""});
-    const [error,setError] = useState("");
-    const [newUser,setNewUser]=useState(false);
+    // useEffect(()=> {
+    //     getInstitutes();
+    // }, []);
 
-    function getInstitutes() {
-        setLoading(true);
-        // const querySnapshot = await getDocs(collection(db, "users"));
-        // querySnapshot.forEach((doc) => {
-        //     console.log(`${doc.id} => ${doc.data()}`);
-        // });
-        onSnapshot(collection_query,(querySnapshot)=>{
-            const items = [];
-            querySnapshot.forEach((doc) => {
-                items.push(doc.data());
-            });
-            setInstitutes(items);
-        setLoading(false);
-        })
-    }
-
-    useEffect(()=> {
-        getInstitutes();
-    }, []);
-
-    if (loading){
-        return <h1>Loading...</h1>
-    }
-
-
-
-    const adminUser={
-        email:"admin@admin.com",
-        password:"admin123"
-    }
-
-    const Login = details=>{
-        console.log(details);
-        if (details.email === adminUser.email && details.password === adminUser.password){
-            console.log("Logged in")
-            setUser({
-                name:"Stav",
-                email: details.email
-            })
-        }
-        else{
-            console.log("Details do not match")
-        }
-
-    }
-    const Logout = ()=>{
-        // console.log('logout');
-        setUser({email:"",password:""});
-    }
-    const new_user= ()=>{
-        setNewUser(true);
-        console.log('yess');
-    }
+    // if (loading){
+    //     return <h1>Loading...</h1>
+    // }
+    // function ForwardsLabel({ direction }) {
   return (
 
     <div className="App">
-        {/*<h1>Blah Blah</h1>*/}
-        {/*{institutes.map((institute)=>(*/}
-        {/*    <h2>{institute.hello}</h2>*/}
-        {/*    ))}*/}
-        {(user.email!=="") ? (
-            <div className="welcome">
-                <h2>Welcome,<span>{user.name}</span></h2>
-                <button onClick={Logout}>Logout</button>
-                <Topbar/>
-
-                 <div className="container">
-                      <div className="patients">
-
-                      </div>
-
-                     {/*<span></span>*/}
-
-                     <div className="containerRight">
-                         <div className="Sidebar">
-
-                             <Router>
-                                 <Sidebar/>
-                                 <Routes>
-                                 <Route path="/dana_barger" element={<Patient />} />
-                                 </Routes>
-                             </Router>
-
-
-                         </div>
-                     </div>
-                 </div>
-            </div>
-
-        ):
-            (newUser === true) ? (
-                <div>
-                                    <NewUser/>
-                </div>
-            ):
-            (
-            <LoginFrom Login={Login} error={error} new_user={new_user}/>
-        )
-        }
-
-      {/*<Topbar/>*/}
-
-      {/* <div className="container">*/}
-      {/*      <div className="patients">*/}
-
-      {/*      </div>*/}
-
-      {/*     /!*<span></span>*!/*/}
-
-      {/*     <div className="containerRight">*/}
-      {/*         <div className="Sidebar">*/}
-
-      {/*             <Router>*/}
-      {/*                 <Sidebar/>*/}
-      {/*                 <Routes>*/}
-      {/*                 <Route path="/dana_barger" element={<Patient />} />*/}
-      {/*                 </Routes>*/}
-      {/*             </Router>*/}
-
-
-      {/*         </div>*/}
-      {/*     </div>*/}
-      {/* </div>*/}
-
-
+        {/*Forwards*/}
+        {direction === DIRECTIONS.RTL && <img src="arrow-left.png" />}
+        {/*<Secretary signUser={signUser}  new_user={newUser} new_patients={newPatients} variant={false}/>*/}
+    {/*<AddMeetingSummaries/>*/}
+    {/*    <Chat userUid1="Rahbt7jhvugjFSsnrcnBb5VMfUb2" userUid2="Rahbt7jhvugjFSsnrcnBb5VMfUb2"/>*/}
+      <LoginFrom />
     </div>
 
   );
+
 }
+    // ForwardsLabel.propTypes = {
+    //     ...withDirectionPropTypes,
+    // };
 export default App;
+// export default withDirection(App);
 
 

9ca8714e653cd5236799fa87879871cb78601c33
stav <stav45451998@waal.com>
2022-07-26 17:41:51 +0300

    add comments and clear

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,11 @@
 import {initializeApp} from "firebase/app"
 import {getStorage} from "firebase/storage";
 import {
-    addDoc,
     collection,
-    deleteDoc,
     doc,
     getDoc,
     getDocs,
-    getFirestore, limit, onSnapshot,
-    orderBy,
+    getFirestore,
     query,
     setDoc,
     updateDoc,
@@ -19,35 +16,26 @@ import {
     getAuth,
     sendEmailVerification,
     sendPasswordResetEmail,
-    sendSignInLinkToEmail,
     signInWithEmailAndPassword,
     signOut,
     updateEmail,
     updatePassword,
     setPersistence, browserSessionPersistence
 } from "firebase/auth";
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import {firebaseConfig} from './firebaseConfigKey'
 import firebase from "firebase/compat/app";
 import 'firebase/compat/firestore';
-// import firebase from "firebase/compat";
 import {makePassword} from "./useFunction"
 import hash from "hash.js";
-import {useEffect, useState} from "react";
 
-// TODO: Add SDKs for Firebase products that you want to use
+
+
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyCszwP0WK5xg5HYtaz5efKrQ5AYen8BnHA",
-    authDomain: "portal-website-db707.firebaseapp.com",
-    projectId: "portal-website-db707",
-    storageBucket: "portal-website-db707.appspot.com",
-    messagingSenderId: "1028746257758",
-    appId: "1:1028746257758:web:cf5ebe0124dc12f5499bcf",
-    measurementId: "G-RLJD2L3NRQ"
-};
+
+
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig)
@@ -158,7 +146,6 @@ export const addUserFromExternalAgent = async details => {
 export const addUserFromAdmin = async (details, institute) => {
     // create the user
     let uid_user = await addUserFromExternalAgent({
-        // TODO: need to add external in the institute?
         ...details, institutes: {[institute]: [],external:[]}, license: "משרד החינוך",
         titles: ["therapist"], institute: "", childrenIds: []
     })
@@ -355,7 +342,6 @@ export const addPatientToExternalTherapist = async (id, code, connection) => {
         ), {
             active: true,
             fromDate:firebase.firestore.Timestamp.fromDate(new Date()),
-            //TODO:add a connection
             connection: connection,
             institute: 'external',
         })
@@ -506,7 +492,6 @@ export const deletePatientFromInstitute = async (institute, removeOb) => {
 
         return flag
     } catch (err) {
-        console.log(err)
         return false
     }
 
